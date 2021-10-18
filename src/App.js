b72141c21fee6392328b4cb99120a1d1339e1968
Ricardo Garrido <ricardoa.garridop@gmail.com>
2021-10-18 20:25:04 -0300

    aplicacion de nav en header

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-const App = () => "Hola Mundo String Prueba";
+const App = () => {
+return(
+    <header>
+        <h1>Soy el titulo principal</h1>
+        <nav>
+            <a href="#">link1</a>
+            <a href="#">Link2</a>
+        </nav>
+    </header>
+)
+};
 
 export default App
